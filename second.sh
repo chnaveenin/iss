05baa88c044b3a257bf36dea3d4d861bff0a5c08
chnaveenin <chnaveen.in@gmail.com>
2022-04-05 10:24:42 +0530

    modified: second.sh

--- second.sh
+++ second.sh
@@ -4,3 +4,16 @@ numbers=(2021101025 2021101015 2021101011)
 echo $numbers
 echo ${numbers[1]}
 echo ${numbers[2]}
+declare -a names
+names=(Naveen Adithya Lakshman)
+echo ${names[*]}
+echo ${names[@]}
+echo ${names[0]}
+echo ${#names[*]}
+echo ${#names[2]}
+echo ${names[1]:2}
+echo ${names[1]:2:4}
+echo ${names[*]/a/*}
+unset names[1]
+echo ${names[*]}
+
